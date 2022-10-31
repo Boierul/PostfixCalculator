fe5e46f7a0a78361d5532f86a9a77493f635820e
Boierul <49337867+Boierul@users.noreply.github.com>
2022-10-31 21:10:58 +0100

    Updated Client JavaDocs

--- src/Client.java
+++ src/Client.java
@@ -4,7 +4,16 @@ import Structures.Abstract.Token;
 
 import java.util.ArrayList;
 
+/**
+ * Class that represents the end-user
+ */
 public class Client {
+    /**
+     * Method that evaluates the validity of the mathematical expression
+     *
+     * @param tokenList the
+     * @return the result of the operation || 0 (if the calculator has no operations)
+     */
     public int evaluateExpression(ArrayList<Token> tokenList) {
         CalculatorVisitor calculatorVisitor =
                 new CalculatorVisitor();
